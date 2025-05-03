1b3c6cd9d73c7aa3063b36d5e345682aeac4d0c9
tracer-ysyx <tracer@ysyx.org>
2025-05-03 17:16:12 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:12 up  6:46,  1 user,  load average: 0.07, 0.06, 0.04

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -53,13 +53,24 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
   return -1;
 }
+
 //单步si
-static int cmd_si(char *argc){
+static int cmd_si(char *args){
   char *arg = strtok(NULL, " ");
-  int n = (int)*arg;
-  cpu_exec(n);
-  return 0;
-}
+  
+  if(args==NULL){
+    cpu_exec(1);
+  }else{
+    int n = atoi(arg);
+    if(n<=0){
+        printf("ERROR!!!");
+        n = 1;
+    }
+    cpu_exec(n);
+  }
+    return 0;
+  }
+ 
 //help
 static int cmd_help(char *args);
 //info
