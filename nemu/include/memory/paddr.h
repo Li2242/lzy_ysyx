351bcc16ca687a13ffcdaca5908ffa76909324ca
tracer-ysyx <tracer@ysyx.org>
2025-05-07 19:27:28 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:27:28 up 12:42,  1 user,  load average: 0.87, 0.37, 0.14

--- nemu/include/memory/paddr.h
+++ nemu/include/memory/paddr.h
@@ -18,13 +18,19 @@
 
 #include <common.h>
 
+//起始地址，paddr_t通常表示物理地址
 #define PMEM_LEFT  ((paddr_t)CONFIG_MBASE)
+//中止地址
 #define PMEM_RIGHT ((paddr_t)CONFIG_MBASE + CONFIG_MSIZE - 1)
+//复位向量
 #define RESET_VECTOR (PMEM_LEFT + CONFIG_PC_RESET_OFFSET)
 
 /* convert the guest physical address in the guest program to host virtual address in NEMU */
+//将 guest 程序中的客户物理地址转换为 NEMU 中的主机虚拟地址
 uint8_t* guest_to_host(paddr_t paddr);
+
 /* convert the host virtual address in NEMU to guest physical address in the guest program */
+//* 将NEMU中的主机虚拟地址转换为 guest 程序中的客户物理地址 */
 paddr_t host_to_guest(uint8_t *haddr);
 
 static inline bool in_pmem(paddr_t addr) {
@@ -32,6 +38,7 @@ static inline bool in_pmem(paddr_t addr) {
 }
 
 word_t paddr_read(paddr_t addr, int len);
+
 void paddr_write(paddr_t addr, int len, word_t data);
 
 #endif
