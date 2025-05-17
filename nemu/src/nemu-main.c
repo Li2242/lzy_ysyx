7f49803a383fa155370f89c9f74059eefd6f4b1f
tracer-ysyx <tracer@ysyx.org>
2025-05-17 19:57:15 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:15 up  6:07,  1 user,  load average: 0.09, 0.08, 0.02

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -48,8 +48,12 @@ int main(int argc, char *argv[]) {
   while(fgets(line,512,fp)!= NULL){
     line_num++;
     if(line[0] == '\n')continue;
-
+    char *temp = 0;
     char *expression = strchr(line,' ');
+    int len = strlen(line) - strlen(expression);
+    strncpy(temp,line,len);
+    temp[len] = '\0';
+    uint32_t num = (unsigned int)atoi(temp); 
     if(expression == NULL){
       printf("Invida format in %d lien\n",line_num);
       continue;
@@ -70,6 +74,9 @@ int main(int argc, char *argv[]) {
     uint32_t result = expr(expression,&r);
      //printf("进入expr后\n");
     printf("r=%d result=%u\n",r,result);
+    if(num == result){
+      printf("对了！%d\n",line_num);
+    }
   }
   fclose(fp);
 
