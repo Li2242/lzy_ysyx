193d8d6d626c5a479670afb278dbe01022f30340
tracer-ysyx <tracer@ysyx.org>
2025-05-17 16:43:59 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:43:59 up  2:53,  1 user,  load average: 0.20, 0.06, 0.01

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -42,10 +42,10 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   printf("打开文件了\n");
-  char line[40];
+  char line[32];
   int line_num = 0;
   //开始读文章了
-  while(fgets(line,40,fp)!= NULL){
+  while(fgets(line,32,fp)!= NULL){
     line_num++;
     if(line[0] == '\n')continue;
 
