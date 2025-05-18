f0f25796a321e92f231d29e21e634e93ea85ce61
tracer-ysyx <tracer@ysyx.org>
2025-05-18 17:43:06 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:43:06 up  8:06,  1 user,  load average: 0.04, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -237,7 +237,7 @@ word_t eval(int p,int q) {
       case '/': 
         if(val2 == 0){
           printf("Error: Division by zero\n");
-          
+          assert(0);
         }
         return val1/val2;
       default: assert(0);
