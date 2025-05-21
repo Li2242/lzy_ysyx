4b6a2f2f613dceae8572c9ad2e1ec07c6d8785d8
tracer-ysyx <tracer@ysyx.org>
2025-05-21 21:33:31 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:33:31 up  6:59,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -18,14 +18,21 @@
 #define NR_WP 32
 
 typedef struct watchpoint {
+  //表示监视点的序号
   int NO;
+  //下一个监视点的地址
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
 
 } WP;
 
+WP* new_wp();
+void free_wp(WP *wp);
+
 static WP wp_pool[NR_WP] = {};
+//head用于组织使用中的监视点结构
+//free_用于组织空闲的监视点结构
 static WP *head = NULL, *free_ = NULL;
 
 void init_wp_pool() {
@@ -40,4 +47,42 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(){
+  if(free_ == NULL) return NULL;
 
+  WP *p = free_;
+  free_ = free_->next;
+  //把p加在了整个使用链表的头部
+  p->next = head;
+  head = p;
+
+  return p;
+  
+}
+
+void free_wp(WP *wp){
+  if(wp == NULL) return;
+  //从head中删除
+  WP** pp = &head;
+  while(*pp != NULL){
+    //找到目标地址
+    if(*pp == wp){
+      //跳过目标地址
+      /*假设我们有一个链表：A -> B -> C，现在要删除节点 B。
+        第一次匹配失败
+        现在是第二次
+        pp 是指向 A.next 的指针
+        *pp 就是 pp 所指向的内容，也就是 A.next 本身。
+        pp = wp->next 相当于把 C 的地址赋值给 A.next
+      */
+      *pp = wp->next;
+      break;
+    }
+    //pp是结构体中next的指针
+    pp = &((*pp)->next);
+  }
+
+  //加入free_中
+  wp->next = free_;
+  free_ = wp;
+}
