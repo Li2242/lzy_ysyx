4fc289d55851dedf66fbbc352db351da4384a596
tracer-ysyx <tracer@ysyx.org>
2025-06-08 10:44:12 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:44:12 up  1:03,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,6 +40,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   
+  //在Kconfig中可以控制这个宏是否生成
   //扫描监视点
   #ifdef CONFIG_WATCHPOINT
   bool success = false;
