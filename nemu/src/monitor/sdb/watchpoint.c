3094fbf7f3e4c627b38f8366bd61a96f3ebaba1d
tracer-ysyx <tracer@ysyx.org>
2025-06-09 20:03:46 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:03:46 up  4:42,  1 user,  load average: 0.17, 0.12, 0.03

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -125,12 +125,13 @@ void scan_watchpoints(bool* success){
   }
 }
 
+//扫描监视点
 void scan(){
   WP *wp = head;
     //是否有监视点被触发
     while(wp!=NULL){
-      //这个值修改了6.7
-        printf("监视点 %d: %s 的值 0x%08x\n",wp->NO, wp->s,wp->n );
+        // printf("Watchpoint %d: %s 的值 0x%08x\n",wp->NO, wp->s,wp->n );
+        printf("The value of the %d monitoring point %s is 0x%08x\n",wp->NO,wp->s,wp->n);
         wp = wp->next;
     }
     if(wp == NULL){
