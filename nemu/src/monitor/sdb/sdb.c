9c9d515edc61b55ee9601d90722a2b98b22a6403
tracer-ysyx <tracer@ysyx.org>
2025-07-08 10:18:08 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:18:08 up 10:06,  1 user,  load average: 0.33, 0.18, 0.06

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -249,15 +249,8 @@ static int cmd_x(char *args){
     Log("Length must be a positive integer.\n");
     return 1;
   }else{
-    for(int i = 0; i<n;){
-      printf("0x%08x: ",addr);
-      for(int k=0;k<4;k++){
-        printf("0x%08x ",vaddr_read(addr,4));
-        addr += 4;
-        i++;
-        //防止出现多打印的情况
-        if(i>=n) break;
-      }
+    for(int i = 0; i<n;i++){
+      printf("0x%08x: 0x%08x",addr,vaddr_read(addr,4));
       printf("\n");
     }
   }
