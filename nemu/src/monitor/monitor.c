ac1551a915c7245b56b81db97a9e27e3a70c9c29
tracer-ysyx <tracer@ysyx.org>
2025-07-08 10:24:03 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:24:03 up 10:12,  1 user,  load average: 0.10, 0.11, 0.07

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -56,6 +56,7 @@ static char *diff_so_file = NULL;
 static char *img_file = NULL;
 static int difftest_port = 1234;
 
+//这个函数会将一个有意义的客户程序从镜像文件读入到内存, 覆盖刚才的内置客户程序. 
 static long load_img() {
   if (img_file == NULL) {
     Log("No image is given. Use the default build-in image.");
