441fa7e30746d0a251de91ab510e9e0480aa0e99
tracer-ysyx <tracer@ysyx.org>
2025-07-10 14:41:49 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:41:49 up  6:23,  1 user,  load average: 0.00, 0.06, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -310,9 +310,9 @@ word_t eval(int p,int q,bool *success) {
         case '+': return val1 + val2;
         case '-': return val1 - val2; 
         case '*': return val1 * val2;
-        case TK_EQ: return val1 == val2;
-        case TK_UEQ: return val1 != val2;
-        case TK_H: return val1 && val2;
+        case TK_EQ: *success = 1;return val1 == val2;
+        case TK_UEQ: *success = 1; return val1 != val2;
+        case TK_H: *success = 1; return val1 && val2;
         case '/': 
           if(val2 == 0){
             Log("Error: Division by zero\n");
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -115,13 +115,13 @@ void free_wp(int n){
 void scan_watchpoints(bool* success){
   WP *wp = head;
   while(wp != NULL){
-    bool success0 = true;
+    bool success0 = false;
     uint32_t a = expr(wp->s,&success0);
-    if(success0 == false){
-      Log("Evaluation failed at the watchpoint!\n ");
-      wp = wp->next;
-      return;
-    }
+    // if(success0 == false){
+    //   Log("Evaluation failed at the watchpoint!\n ");
+    //   wp = wp->next;
+    //   return;
+    // }
     if(a != wp->n){
       printf("The watchpoint %s was triggered,and its value changed from 0x%08x to 0x%08x.\n",wp->s,wp->n,a);
       wp->n = a;
@@ -143,7 +143,6 @@ void scan(){
       Log(" Watchpoints was not been set up!");
     }
     while(wp!=NULL){
-        // printf("Watchpoint %d: %s 的值 0x%08x\n",wp->NO, wp->s,wp->n );
         printf("The value of the %d Watchpoint %s is 0x%08x\n",wp->NO,wp->s,wp->n);
         wp = wp->next;
     } 
