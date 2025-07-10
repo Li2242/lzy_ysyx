14ce3e5de5318c38ea2082a167ee7c81055d4f0d
tracer-ysyx <tracer@ysyx.org>
2025-07-10 14:51:28 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:28 up  6:33,  1 user,  load average: 0.05, 0.06, 0.08

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -129,7 +129,7 @@ void scan_watchpoints(bool* success){
       *success = true;
     }
     if(success0 == true && a == 1){
-      printf("Condition expression triggered! 0x%08x.\n",a);
+      printf("Condition expression triggered! %d.\n",a);
       *success = true;
     }
     wp = wp->next;
