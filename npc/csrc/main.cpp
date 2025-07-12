4829fbbac8cfbbdf354ba48964c1dd0b953b9969
tracer-ysyx <tracer@ysyx.org>
2025-07-12 21:45:03 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:03 up 13:51,  1 user,  load average: 0.24, 0.12, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -61,6 +61,12 @@ int main(int argc,char** argv) {
     tfp->dump(contextp->time());    // 记录波形
     contextp->timeInc(1);
     printf( "sum = %d pc = %x\n",top->sum,top->pc);
+
+    top->clk = 1;
+    top->eval();
+    tfp->dump(contextp->time());    // 记录波形
+    contextp->timeInc(1);
+    printf( "sum = %d pc = %x\n",top->sum,top->pc);
   }
   sim_end();
   return 0;
