f147b6b49fd24f64a2c77321f250fc03b0da08f1
tracer-ysyx <tracer@ysyx.org>
2025-07-13 10:17:28 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:17:28 up 15:53,  1 user,  load average: 0.03, 0.05, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -9,12 +9,19 @@ VerilatedVcdC* tfp = NULL;
 static Vnpc* top;
 
 void sim_end();
-
+#define CYCKLE_NUM 10
 #define IN_ADDRESS 0x80000000
-__uint32_t memory[3] = {
-  0x00500093,
-  0x00A00113,
-  0xFFD00193
+__uint32_t memory[CYCKLE_NUM] = {
+    0x00100093,  // 1: addi x1, x0, 1      (x1 = 0 + 1 = 1)
+    0x06400113,  // 2: addi x2, x0, 100    (x2 = 0 + 100 = 100)
+    0xFFB00193,  // 3: addi x3, x0, -5     (x3 = 0 - 5 = -5)
+    0x0FF00213,  // 4: addi x4, x0, 255    (x4 = 0 + 255 = 255)
+    0x00A08293,  // 5: addi x5, x1, 10     (x5 = 1 + 10 = 11)
+    0xFFE14313,  // 6: addi x6, x5, -2     (x6 = 11 - 2 = 9)
+    0x00000393,  // 7: addi x7, x0, 0      (x7 = 0 + 0 = 0)
+    0x00140413,  // 8: addi x8, x8, 1      (x8 = x8 + 1 = 1)
+    0x3FF00493,  // 9: addi x9, x0, 1023   (x9 = 0 + 1023 = 1023)
+    0xFFF00513   // 10: addi x10, x0, -1   (x10 = 0 - 1 = -1)
 };
 
 
@@ -48,7 +55,7 @@ int main(int argc,char** argv) {
     tfp->dump(contextp->time()); // 记录复位前状态
     contextp->timeInc(10);
 
-    for(int i = 0; i<3; i++){
+    for(int i = 0; i < CYCKLE_NUM; i++){
 
     top->clk = 0;
     top->inst = pmem_read(top->pc);
