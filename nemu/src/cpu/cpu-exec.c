fb1db7c570eef7f890bf8e65a7489e61456f05a3
tracer-ysyx <tracer@ysyx.org>
2025-07-18 07:59:17 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:59:17 up 21 min,  1 user,  load average: 0.47, 0.42, 0.21

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -122,7 +122,7 @@ static void execute(uint64_t n) {
         int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
                 (nemu_state.state == NEMU_QUIT);
         if(good == 0){
-            log_write("下面是临近出错时的8条指令");
+            log_write("下面是临近出错时的8条指令\n");
             for(int j=0; j<8; j++){
                 if(ring_buf_count<8){
                     log_write("%s\n", ring_buf[ring_buf_count++]);
