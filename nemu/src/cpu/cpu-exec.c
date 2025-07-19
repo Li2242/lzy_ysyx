421fd692b250c2a3b6f6e739bbde4344843609c3
tracer-ysyx <tracer@ysyx.org>
2025-07-19 15:02:37 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:37 up 10:37,  1 user,  load average: 0.09, 0.10, 0.08

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -118,7 +118,8 @@ static void execute(uint64_t n) {
         // if(strncmp(temp,"jal",3) ==0|| strncmp(temp,"jalr",4)==0){
         //     printf("%s\n",temp);
         // }
-        printf("%s\n",temp);
+        int l = strlen(temp);
+        printf("%d, %s\n",l,temp);
     g_nr_guest_inst ++;  //对一个用于记录客户指令的计数器加1
     trace_and_difftest(&s, cpu.pc);
 
