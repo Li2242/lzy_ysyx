782c6b89a93400e7ed1a8e91ca1651dcd967809b
tracer-ysyx <tracer@ysyx.org>
2025-07-22 15:50:56 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:50:56 up  7:58,  1 user,  load average: 0.00, 0.01, 0.01

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -2,26 +2,27 @@
 module alu(
     input[31:0] imm,
     input[31:0] src1,
-    output[31:0]  pc,
     input[31:0] alu_op,
+    input[31:0] pc,
+    output[31:0]  next_pc,
     output[31:0] result
 );
 
-wire result_auipc[31:0];
-wire result_lui[31:0];
-wire result_jal[31:0];
-wire result_jalr[31:0];
-wire result_addi[31:0];
+wire [31:0] result_auipc;
+wire [31:0] result_lui;
+wire [31:0] result_jal;
+wire [31:0] result_jalr;
+wire [31:0] result_addi;
 //jal
-assign pc = alu_op[2] ? pc+imm : pc;
+assign next_pc = alu_op[2] ? pc+imm : pc;
 //jalr
-assign pc = alu_op[3] ? (src1+imm)&~1 : pc ;
+assign next_pc = alu_op[3] ? (src1+imm)&~1 : pc ;
 
 assign result_auipc = imm + pc;
 assign result_lui   = imm;
 assign result_jal   = pc + 4;
 assign result_jalr  = pc + 4;
-assign result_addi   = src1 + imm;
+assign result_addi  = src1 + imm;
 
 assign result = ({32{alu_op[0]}} & result_auipc)
               | ({32{alu_op[1]}} & result_lui)
--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -14,7 +14,7 @@ module npc(
 Reg#(32,32'h80000000) pc_4(
     .clk 	 (clk  ),
     .rst     (rst),
-    .din     (pc+32'h4),
+    .din     (next_pc),
     .dout    (pc),
     .wen     (1)
 );
@@ -25,15 +25,16 @@ end
 
 //内部信号定义
 wire[6:0] opcode;
-wire [31:0]  imm;
+wire[31:0]next_pc;
+wire[31:0]  imm;
 wire[2:0] funct3;
-wire [31:0]  src1;
+wire[31:0]  src1;
 // wire [31:0]  src2;
-wire [4:0]   rs1;
+wire[4:0]   rs1;
 // wire [4:0] 	 rs2;
-wire [4:0]   rd;
+wire[4:0]   rd;
 wire         reg_wen;
-wire alu_op[31:0];
+wire[31:0] alu_op;
 
 //指令BIG类型
 // wire is_R;
@@ -45,18 +46,19 @@ wire is_J;
 wire inst_type;
 
 //立即数
-// wire imm_R[31:0];
-wire imm_I[31:0];
-// wire imm_S[31:0];
-// wire imm_B[31:0];
-wire imm_U[31:0];
-wire imm_J[31:0];
+// wire [31:0]imm_R;
+wire [31:0]imm_I;
+// wire [31:0]imm_S;
+// wire [31:0]imm_B;
+wire [31:0]imm_U;
+wire [31:0]imm_J;
 
 //指令类型
 wire is_auipc;
 wire is_lui;
 wire is_jal;
 wire is_jalr;
+wire is_addi;
 
 
 
@@ -68,10 +70,10 @@ assign opcode  = inst[6:0];
 assign imm_I = {{20{inst[31]}},inst[31:20]};
 // assign imm_S = {{20{inst[31]}},inst[31:25],inst[11:7]};
 // assign imm_B = {{19{inst[31]}},instead[31],inst[7],inst[30:25],inst[11:8],0};
-assign imm_U = {inst[31:12],{12{0}}};
-assign imm_J = {{11{inst[31]}},inst[31],inst[19:12],inst[20],inst[30:21],0};
-assign r1    = inst[19:15];
-assign r2    = inst[24:20];
+assign imm_U = {inst[31:12],{12{1'b0}}};
+assign imm_J = {{11{inst[31]}},inst[31],inst[19:12],inst[20],inst[30:21],1'b0};
+assign rs1    = inst[19:15];
+// assign r2    = inst[24:20];
 assign rd    = inst[11:7];
 assign funct3 = inst[14:12];
 
@@ -109,20 +111,22 @@ assign is_J = (hot_opcode[111]) ? 1 : 0;
 
 //立即数
 assign imm = ({32{is_I}} & imm_I)
-					 | ({32{is_U}} & imm_U)
-					 | ({32{is_J}} & imm_J);
+			| ({32{is_U}} & imm_U)
+		    | ({32{is_J}} & imm_J);
 						//  | ({32{is_S}} & imm_S);
 
 //判断指令类型
 assign is_auipc = is_U & hot_opcode[23];
+assign is_lui   = is_U & hot_opcode[55];
 assign is_jal   = is_J ;
 assign is_jalr  = is_I & hot_funct3[0] & hot_opcode[103];
 assign is_addi  = is_I & hot_funct3[0] & hot_opcode[19];
 
 assign alu_op[0] = is_auipc;
-assign alu_op[1] = is_jal;
-assign alu_op[2] = is_jalr;
-assign alu_op[3] = is_addi;
+assign alu_op[1] = is_lui;
+assign alu_op[2] = is_jal;
+assign alu_op[3] = is_jalr;
+assign alu_op[4] = is_addi;
 //读取数据
 //符号扩
 // wire is_addi = (opcode == 7'b0010011) && (funct3 == 3'b000);
@@ -136,9 +140,10 @@ assign alu_op[3] = is_addi;
 alu u_alu(
     .imm    	(imm     ),
     .src1   	(src1    ),
-    .pc     	(pc      ),
     .alu_op 	(alu_op  ),
-    .result 	(result  )
+    .pc         (pc),
+    .next_pc    (next_pc ),
+    .result 	(alu_result  )
 );
 
 
