6adca30d9b69504b9844dc0d40620cbdaeb715a9
tracer-ysyx <tracer@ysyx.org>
2025-07-25 20:58:58 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:58 up 11:06,  1 user,  load average: 0.32, 0.14, 1.31

--- npc/csrc/watchpoint.cpp
+++ npc/csrc/watchpoint.cpp
@@ -107,7 +107,6 @@ void scan_watchpoints(bool* success){
     //   wp = wp->next;
     //   return;
     // }
-		printf("%d\n",a);
     if(a != wp->n && success0 == false){
       printf("The watchpoint %s was triggered,and its value changed from 0x%08x to 0x%08x.\n",wp->s,wp->n,a);
       wp->n = a;
