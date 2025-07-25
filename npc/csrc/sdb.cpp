159a89f81bbb42d06e4d0ea02a82708c9eda7736
tracer-ysyx <tracer@ysyx.org>
2025-07-25 16:00:20 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:00:20 up  6:07,  1 user,  load average: 0.15, 0.10, 0.09

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -145,7 +145,7 @@ static int cmd_x(char *args){
 	}
 	uint32_t addr;
 	sscanf(arg[1],"%u",&addr);
-	// printf("0x%08x",addr);
+	printf("addr = 0x%08x\n",addr);
 	for(int i =0;i < n;i++){
 		printf("addr = 0x%08x: 0x%08x\n",addr,pmem_read(addr,4));
 		addr += 4;
