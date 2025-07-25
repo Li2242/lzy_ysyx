a867b902b240e0a387efbb9207984c00b167de3b
tracer-ysyx <tracer@ysyx.org>
2025-07-25 14:40:16 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:16 up  4:47,  1 user,  load average: 0.10, 0.19, 0.14

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -5,6 +5,13 @@
 
 //批处理模式
 static int is_batch_mode = false;
+//函数
+static int cmd_c  (char *args);
+static int cmd_q  (char *args);
+static int cmd_si (char *args);
+//help
+static int cmd_help(char *args);
+
 
 //使用 readline 函数从标准输入读取用户输入的一行命令，如果不为空,将其添加到历史记录中
 //并返回给调用的参数
@@ -22,17 +29,8 @@ static char* rl_gets(){
 	return line_read;
 }
 
-static int cmd_c(char *args){
-	sim_exe(-1);
-	return 0;
-}
 
-static int cmd_q(char *args){
-		return -1;
-}
 
-//help
-static int cmd_help(char *args);
 
 static struct{
 	const char* name;
@@ -50,16 +48,18 @@ static struct{
 void sdb_set_batch_mode() {
   is_batch_mode = true;
 }
+
 //npc主循环
 void sdb_mainloop(){
+	//批处理模式
 	if(is_batch_mode){
 		cmd_c(NULL);
 		return;
 	}
-
+	//循环
 	for(char *str;(str = rl_gets()) != NULL; ){
 		char *str_end = str+strlen(str);
-
+		//这里是首次发出对输入命令的切割
 		char *cmd = strtok(str," ");
 		if(cmd == NULL){continue;}
 
@@ -97,5 +97,29 @@ static int cmd_help(char* str){
 	}
 	return 0;
 }
+//执行完程序
+static int cmd_c(char *args){
+	sim_exe(-1);
+	return 0;
+}
 
+//退出
+static int cmd_q(char *args){
+		return -1;
+}
 
+static int cmd_si(char *args){
+	char* arg = strtok(NULL," ");
+	if(arg == NULL){
+		sim_exe(1);
+		return 0;
+	}
+	int n;
+	sscanf("arg","%d",&n);
+	if(n<=0){
+		printf("请输入大于0的执行次数");
+		return 0;
+	}
+	sim_exe(n);
+	return 0;
+}
