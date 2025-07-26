62974e252e6a612dc40ea0ef990f1e0cd547dd1b
tracer-ysyx <tracer@ysyx.org>
2025-07-26 11:02:45 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:02:45 up  2:09,  1 user,  load average: 0.00, 0.82, 1.32

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -8,7 +8,7 @@ extern "C" void ebreak(uint32_t pc){
 }
 
 int main(int argc,char** argv) {
-	//解析参数（现在的功能只有读入bin文件）
+	//解析参数(现在的功能只有读入bin文件)
   parse_args(argc, argv);
 	//初始化
   sim_init(argc,argv);
