771ce2fa271d67373e6f313923c0b0972d816af7
tracer-ysyx <tracer@ysyx.org>
2025-07-27 14:44:29 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:44:29 up  7:05,  1 user,  load average: 3.58, 3.48, 2.61

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -21,7 +21,7 @@
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n) {
   int inst_num = n/4;
 	uint32_t* img = (uint32_t *)buf;
-	Log("准备写了！");
+	Log("准备写了！\n");
 	for(int i =0;i<inst_num;i++){
 		paddr_write(addr,4,*img++);
 	}
