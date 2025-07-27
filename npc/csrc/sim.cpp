ce70e5e35551eaaa35121535fe55f0116ef1e174
tracer-ysyx <tracer@ysyx.org>
2025-07-27 14:47:20 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:20 up  7:07,  1 user,  load average: 1.33, 2.59, 2.41

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -81,7 +81,7 @@ void execute(uint32_t n){
 	g_print_step = n < MAX_INST_TO_PRINT;
   for(int i = 0; (i < n) && npc_state == NPC_RUNNING ; i++){
 		//pc值要正确,这是变化前的pc，后面经过执行就变成下一个pc了
-		int pc = top->pc;
+		uint32_t pc = top->pc;
     top->clk = 0;
     top->inst = pmem_read(top->pc,4);
     top->eval();
