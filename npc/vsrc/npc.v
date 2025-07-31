1a030024ab9b40619c2f783e1b183e0a1abfd178
tracer-ysyx <tracer@ysyx.org>
2025-07-31 18:19:52 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:19:52 up  9:27,  1 user,  load average: 0.20, 0.22, 0.17

--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -226,7 +226,7 @@ assign wmask = is_sb ? 8'b00000001 :
 
 //读地址
 always @(*) begin
-	if(mem_en)begin
+	if(0)begin
 		rdata =  is_lbu ? v_pmem_read(raddr , 1) & 32'hFF:
 							// is_lhu ? v_pmem_read(raddr , 2) & 32'hFFFF:
 					 						 v_pmem_read(raddr , 4);
