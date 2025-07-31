d0945c6eabf2e7c9dfca7fbb2705e5ca931fb671
tracer-ysyx <tracer@ysyx.org>
2025-07-31 18:36:58 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:36:58 up  9:44,  1 user,  load average: 0.37, 0.26, 0.16

--- npc/obj_dir/Vnpc___024root__DepSet_ha668e1b2__0__Slow.cpp
+++ npc/obj_dir/Vnpc___024root__DepSet_ha668e1b2__0__Slow.cpp
@@ -670,12 +670,12 @@ VL_ATTR_COLD void Vnpc___024root___stl_sequent__TOP__0(Vnpc___024root* vlSelf) {
     vlSelfRef.npc__DOT__is_jalr = (IData)((0x67U == 
                                            (0x707fU 
                                             & vlSelfRef.npc__DOT__inst)));
-    vlSelfRef.npc__DOT__is_lbu = (IData)((0x4003U == 
-                                          (0x707fU 
-                                           & vlSelfRef.npc__DOT__inst)));
     vlSelfRef.npc__DOT__is_addi = (IData)((0x13U == 
                                            (0x707fU 
                                             & vlSelfRef.npc__DOT__inst)));
+    vlSelfRef.npc__DOT__is_lbu = (IData)((0x4003U == 
+                                          (0x707fU 
+                                           & vlSelfRef.npc__DOT__inst)));
     vlSelfRef.npc__DOT__src1 = ((0U == (0x1fU & (vlSelfRef.npc__DOT__inst 
                                                  >> 0xfU)))
                                  ? 0U : vlSelfRef.npc__DOT__u_regfile2__DOT__rf
@@ -743,13 +743,13 @@ VL_ATTR_COLD void Vnpc___024root___stl_sequent__TOP__0(Vnpc___024root* vlSelf) {
     vlSelfRef.npc__DOT__reg_from_pc_4 = ((0x6fU == 
                                           (0x7fU & vlSelfRef.npc__DOT__inst)) 
                                          | (IData)(vlSelfRef.npc__DOT__is_jalr));
+    vlSelfRef.npc__DOT__src2_is_imm = ((IData)(vlSelfRef.npc__DOT__is_addi) 
+                                       | (0x17U == 
+                                          (0x7fU & vlSelfRef.npc__DOT__inst)));
     vlSelfRef.npc__DOT__mem_en = ((IData)((0x2003U 
                                            == (0x707fU 
                                                & vlSelfRef.npc__DOT__inst))) 
                                   | (IData)(vlSelfRef.npc__DOT__is_lbu));
-    vlSelfRef.npc__DOT__src2_is_imm = ((IData)(vlSelfRef.npc__DOT__is_addi) 
-                                       | (0x17U == 
-                                          (0x7fU & vlSelfRef.npc__DOT__inst)));
     vlSelfRef.npc__DOT__raddr = (vlSelfRef.npc__DOT__src1 
                                  + vlSelfRef.npc__DOT__imm);
     vlSelfRef.alu_result = ((- (IData)(((IData)(vlSelfRef.npc__DOT__is_add) 
@@ -780,4 +780,13 @@ VL_ATTR_COLD void Vnpc___024root___stl_sequent__TOP__0(Vnpc___024root* vlSelf) {
             : ([&]() {
                 Vnpc___024unit____Vdpiimwrap_v_pmem_read_TOP____024unit(vlSelfRef.npc__DOT__raddr, 4U, vlSelfRef.__Vfunc_v_pmem_read__4__Vfuncout);
             }(), vlSelfRef.__Vfunc_v_pmem_read__4__Vfuncout));
+    if (VL_UNLIKELY((vlSelfRef.npc__DOT__mem_en))) {
+        VL_WRITEF_NX("mem_en=%b, is_lbu=%b, raddr=0x%08x\n",0,
+                     1,vlSelfRef.npc__DOT__mem_en,1,
+                     (IData)(vlSelfRef.npc__DOT__is_lbu),
+                     32,vlSelfRef.npc__DOT__raddr);
+        vlSelfRef.npc__DOT__rdata = vlSelfRef.npc__DOT____VdfgExtracted_hfee1d2e3__0;
+    } else {
+        vlSelfRef.npc__DOT__rdata = 0U;
+    }
 }
