bb16388dbd1901495da57cdacbfc64d3026f4313
tracer-ysyx <tracer@ysyx.org>
2025-07-31 16:36:36 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:36 up  7:44,  1 user,  load average: 0.04, 0.05, 0.07

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -53,16 +53,10 @@ void sim_init(int argc,char** argv){
 	// 1. 复位初始化
 		// 在仿真环境中
 	top->clk = 0;
-	top->reset = 1;  // 先置位复位
+	top->reset = 0;  // 先置位复位
 	top->pc = MBASE; // 0x80000000
 	top->eval();     // 应用复位状态
 
-	// 生成一个完整的时钟周期来确认复位
-	top->clk = 1;
-	top->eval();
-	top->clk = 0;
-	top->reset = 0;  // 释放复位
-	top->eval();
 
 	green_printf("===========================================\n");
 //====================  这里是初始化的结束  ===============
