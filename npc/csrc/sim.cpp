489ba649dab722e0274a2db522dd7a896ce3bc70
tracer-ysyx <tracer@ysyx.org>
2025-07-31 15:18:44 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:18:44 up  6:26,  1 user,  load average: 0.01, 0.14, 0.12

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -51,7 +51,6 @@ void sim_init(int argc,char** argv){
 	top->clk = 0;
 	top->reset = 0;
 	top->pc = MBASE;
-	top->eval();
 	green_printf("===========================================\n");
 
 //====================  这里是初始化的结束  ===============
