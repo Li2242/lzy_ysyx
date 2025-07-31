7e80ec16ae7981b8c9a33d63b74e86ca69bc7be7
tracer-ysyx <tracer@ysyx.org>
2025-07-31 15:48:56 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:48:56 up  6:56,  1 user,  load average: 0.54, 0.33, 0.22

--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -206,18 +206,14 @@ assign wmask = is_sb ? 8'b00000001 :
 //读地址
 always @(posedge clk) begin
 	if(mem_en)begin
-		rdata =  is_lbu ? v_pmem_read(raddr , 1) & 32'hFF:
+		rdata <=  is_lbu ? v_pmem_read(raddr , 1) & 32'hFF:
 							// is_lhu ? v_pmem_read(raddr , 2) & 32'hFFFF:
 					 						 v_pmem_read(raddr , 4);
-	end else begin
-		rdata = 0;
-	end
-end
-//写
-always @(posedge clk)begin
-	if  (mem_wen) begin // 有写请求时
+	end else if  (mem_wen) begin // 有写请求时
       v_pmem_write(waddr, wdata, wmask);
-    end
+    end else begin
+		rdata <= 0;
+	end
 end
 // ========================== 内存读写结束   =====================================
 
