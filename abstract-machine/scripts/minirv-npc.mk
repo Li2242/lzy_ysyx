0b8e9c8082dbdca275d33100a04755ecdba704f5
tracer-ysyx <tracer@ysyx.org>
2025-07-31 10:54:14 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:54:14 up  2:01,  1 user,  load average: 0.00, 0.01, 0.04

--- abstract-machine/scripts/minirv-npc.mk
+++ abstract-machine/scripts/minirv-npc.mk
@@ -8,9 +8,13 @@ CXX = minirv-g++
 
 COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
+MINIFLAGS += -l $(shell dirname $(IMAGE).elf)/minirv-npc-log.txt -e $(IMAGE).elf -d $(NEMU_HOME)/build/riscv32-nemu-interpreter-so
 
 AM_SRCS += riscv/npc/libgcc/div.S \
            riscv/npc/libgcc/muldi3.S \
            riscv/npc/libgcc/multi3.c \
            riscv/npc/libgcc/ashldi3.c \
            riscv/npc/libgcc/unused.c
+
+	
+.PHONY: insert-arg
