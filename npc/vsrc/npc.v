6e309c52894e96c788b858b45be8cebb139e46b5
tracer-ysyx <tracer@ysyx.org>
2025-07-31 18:30:55 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:30:55 up  9:38,  1 user,  load average: 0.10, 0.07, 0.10

--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -254,6 +254,8 @@ always @(posedge clk) begin
         $display("RegWrite: rd=%d,  final_result=0x%08x, mem_en=%b, rdata=0x%08x",
                  rd,  final_result, mem_en, rdata);
     end
+		$display("mem_en=%b,is_lw=%b ,is_lbu=%b",
+                  mem_en,is_lw,is_lbu);
 end
 
 always @(posedge clk) begin
