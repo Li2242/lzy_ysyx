eff19be10947c0e883ba791544d8c3683d6ae982
tracer-ysyx <tracer@ysyx.org>
2025-08-01 14:37:50 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:37:50 up  6:34,  1 user,  load average: 0.00, 0.01, 0.00

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -28,9 +28,12 @@
 
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
 #define KBD_ADDR        (DEVICE_BASE + 0x0000060)
+//实时时钟地址
 #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
 #define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
+//音频地址
 #define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
+//磁盘地址
 #define DISK_ADDR       (DEVICE_BASE + 0x0000300)
 #define FB_ADDR         (MMIO_BASE   + 0x1000000)
 #define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
@@ -45,6 +48,7 @@ extern char _pmem_start;
 
 typedef uintptr_t PTE;
 
+//页面大小
 #define PGSIZE    4096
 
 #endif
