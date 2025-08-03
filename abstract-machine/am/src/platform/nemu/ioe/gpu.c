183819370eb137b6a68a1112fec5f7d3388ea1b1
tracer-ysyx <tracer@ysyx.org>
2025-08-03 16:31:09 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:31:09 up  7:32,  1 user,  load average: 0.07, 0.02, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,6 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-	int i;
-  int w = 400;  // TODO: get the correct width
-  int h = 300;  // TODO: get the correct height
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
 }
 
 //屏幕大小寄存器
@@ -20,11 +14,24 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .vmemsz = inw(VGACTL_ADDR+2) * inw(VGACTL_ADDR) * sizeof(uint32_t)
   };
 }
-
+// AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);  //AM帧缓冲控制器
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 100);
-  }
+  int x = ctl->x;
+	int y = ctl->y;
+	int w = ctl->w;
+	int h = ctl->h;
+	uint32_t *pixels = ctl->pixels;
+	int width = inw(VGACTL_ADDR+2);
+	uint32_t* fb = (uint32_t*)FB_ADDR;
+
+	for (int j = 0; j < h; j++) {       
+		for (int i = 0; i < w; i++) {     
+			int screen_index = (y + j) * width + (x + i);
+			int pixel_index = j * w + i;
+			fb[screen_index] = pixels[pixel_index];
+		}
+	}
+
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
