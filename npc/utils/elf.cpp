1ede1a55c6bdca7ad280cc2933caccd12d048188
tracer-ysyx <tracer@ysyx.org>
2025-08-04 21:46:17 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:46:17 up 13:05,  1 user,  load average: 1.20, 1.11, 1.01

--- npc/utils/elf.cpp
+++ npc/utils/elf.cpp
@@ -87,9 +87,7 @@ void ftrace(char* inst){
         if(strncmp(fun1,"jalr",5)==0){
             in = 1;
             char str_t[10];
-						printf("%x\n",target);
             sprintf(str_t,"%x",target);
-						printf("%s\n",str_t);
             uint32_t jalr_target = reg_str2val_num(rd);
             for(int i =0;i<sym_num;i++){
                 in = 1;
