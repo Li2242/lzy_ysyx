05272b5f173b9afe3cc9e93143adfe63d5e9cecb
tracer-ysyx <tracer@ysyx.org>
2025-08-05 18:43:31 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:31 up 10:00,  1 user,  load average: 0.70, 0.54, 0.48

--- npc/csrc/device/timer.cpp
+++ npc/csrc/device/timer.cpp
@@ -23,7 +23,7 @@ void rtc_io_handler() {
     uint64_t us = get_time();       // 获取当前系统运行时间（单位：微秒）
     rtc_port_base[0] = (uint32_t)us;// 低 32 位写入偏移 0
     rtc_port_base[1] = us >> 32;    // 高 32 位写入偏移 4
-		printf("%d %d",rtc_port_base[1],rtc_port_base[0]);
+		printf("   %d %d",rtc_port_base[1],rtc_port_base[0]);
 }
 
 
