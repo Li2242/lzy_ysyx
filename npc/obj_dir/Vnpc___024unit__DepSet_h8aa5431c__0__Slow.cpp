5d4fda5908bbf0fe20994cfa1f449913e6833b5d
tracer-ysyx <tracer@ysyx.org>
2025-08-05 10:26:07 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:26:07 up  1:42,  1 user,  load average: 0.04, 0.08, 0.09

---
+++ npc/obj_dir/Vnpc___024unit__DepSet_h8aa5431c__0__Slow.cpp
@@ -0,0 +1,12 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vnpc.h for the primary calling header
+
+#include "Vnpc__pch.h"
+#include "Vnpc___024unit.h"
+
+VL_ATTR_COLD void Vnpc___024unit___ctor_var_reset(Vnpc___024unit* vlSelf) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+        Vnpc___024unit___ctor_var_reset\n"); );
+    Vnpc__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+}
