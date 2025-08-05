34ac081cfadce61a1cf5f66deff5456629b5a3d9
tracer-ysyx <tracer@ysyx.org>
2025-08-05 18:09:21 +0800

    > sim RTL 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:09:21 up  9:26,  1 user,  load average: 0.31, 0.14, 0.04

--- npc/csrc/device/timer.cpp
+++ npc/csrc/device/timer.cpp
@@ -2,26 +2,7 @@
 #include <sys/time.h>
 uint32_t *rtc_port_base = NULL;
 
-void rtc_io_handler() {
-    uint64_t us = get_time();       // 获取当前系统运行时间（单位：微秒）
-    rtc_port_base[0] = (uint32_t)us;// 低 32 位写入偏移 0
-    rtc_port_base[1] = us >> 32;    // 高 32 位写入偏移 4
-}
 
-
-
-//设备初始化 init_timer
-void init_timer() {
-  rtc_port_base = (uint32_t *)new_space(8);
-}
-
-
-//当前程序运行多长时间
-uint64_t get_time() {
-  if (boot_time == 0) boot_time = get_time_internal();
-  uint64_t now = get_time_internal();
-  return now - boot_time;
-}
 
 //获取当前系统信息并返回
 static uint64_t get_time_internal() {
@@ -30,4 +11,24 @@ static uint64_t get_time_internal() {
   gettimeofday(&now, NULL);
   uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
   return us;
+}
+
+//当前程序运行多长时间
+uint64_t get_time() {
+  boot_time = get_time_internal();
+  uint64_t now = get_time_internal();
+  return now - boot_time;
+}
+
+
+void rtc_io_handler() {
+    uint64_t us = get_time();       // 获取当前系统运行时间（单位：微秒）
+    rtc_port_base[0] = (uint32_t)us;// 低 32 位写入偏移 0
+    rtc_port_base[1] = us >> 32;    // 高 32 位写入偏移 4
+}
+
+
+//设备初始化 init_timer
+void init_timer() {
+  rtc_port_base = (uint32_t *)new_space(8);
 }
