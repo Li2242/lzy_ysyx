da1add57d14312f1b186fe61e483a59ddbdbb1ac
tracer-ysyx <tracer@ysyx.org>
2025-08-10 12:06:56 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:06:56 up  2:59,  1 user,  load average: 2.69, 1.30, 0.93

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -153,7 +153,7 @@ static int decode_exec(Decode *s) {
 
   //特殊指令
 
-  // INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, isa_raise_intr());
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, isa_raise_intr(0,s->pc));
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
