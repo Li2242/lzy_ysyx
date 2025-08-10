adbd9f52529e258d501b305d0409e9e939bf965b
tracer-ysyx <tracer@ysyx.org>
2025-08-10 15:41:29 +0800

    > compile NEMU 312409070514 李志勇 Linux lzy14-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:41:29 up  6:34,  1 user,  load average: 1.26, 1.05, 0.74

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -24,15 +24,15 @@
 		第二个参数pc应指向导致对比结果不一致的指令, 可用于打印提示信息
 */
 bool is_same = true;
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t npc) {
 	for(int i = 0;i<32;i++){
 		if(cpu.gpr[i] != ref_r->gpr[i]){
 			Log("Mismatch in gpr[%d]: dut=0x%08x, ref=0x%08x\n", i, cpu.gpr[i], ref_r->gpr[i]);
 			is_same = false;
 		}
 	}
-	if(cpu.pc != ref_r->pc){
-		Log("Mismatch in pc: dut=0x%08x, ref=0x%08x\n", cpu.pc, pc);
+	if(npc != ref_r->pc){
+		Log("Mismatch in pc: dut=0x%08x, ref=0x%08x\n", cpu.pc, npc);
 		is_same = false;
 	}
   return is_same;
